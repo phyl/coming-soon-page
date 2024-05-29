90b3a68bc6c1551a6105c088d75fd9fc1bd77d42
phyl <pmhindurwa@yahoo.com>
2024-05-29 23:01:00 +0200

    JS and more CSS

---
+++ script.js
@@ -0,0 +1,34 @@
+function ValidateEmail(inputText)
+{
+var mailformat = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; /* /^[^\s@]+@[^\s@]+\.[^\s@]+$/ */
+if(inputText.value.match(mailformat))
+{
+  document.querySelector(".valid-email").style.display = "block";       
+  document.querySelector(".invalid-email").style.display = "none";
+  inputText.style.border = "2px solid var(--desaturated-red)"; 
+  inputText.style.backgroundImage = "none";
+  document.form1.email.focus();
+  return true;
+}
+
+else
+
+{
+  
+    document.querySelector(".invalid-email").style.display = "block";
+    // document.querySelector(".email-input-invalid").style.display = "block";    
+    document.querySelector(".valid-email").style.display = "none";
+    inputText.style.backgroundImage = "url('/images/icon-error.svg')";
+    inputText.style.border = "2px solid var(--soft-red)"; 
+    inputText.style.backgroundPosition = "80%";
+    inputText.style.backgroundRepeat = "no-repeat";
+    // document.getElementById("email").classList.add(".email-input-invalid");
+    // document.getElementById("email").classList.remove("input");
+  }
+
+
+  // inputText.classList.add("email-input-invalid");
+    document.form1.email.focus();
+    return false;
+}
+
